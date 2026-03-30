eb3d0ff38fc7c17b03b59f4449518835e206344a
Nuriabl13 <nuria13balderas2l@gmail.com>
2026-03-30 12:09:50 +0200

    Movido a estructura correspondiente y creacion de estructura de SpriteGameObject

---
+++ src/bcp/framework/consola/SpriteGameObject.java
@@ -0,0 +1,62 @@
+package bcp.framework.consola;
+
+import bpc.daw.consola.Sprite;
+
+import java.awt.*;
+
+public class SpriteGameObject extends GameObject {
+
+    private Sprite sprite;
+    private Image imagen;
+    private Point puntoInicial;
+
+    public SpriteGameObject(Image img, int x, int y){
+
+    }
+
+    public int getX(){
+        return 0;
+    }
+
+    public int getY(){
+        return 0;
+    }
+
+    public int getAnchura(){
+        return 0;
+    }
+
+    public int getAltura(){
+        return 0;
+    }
+
+    public void moverX(int cx){
+
+    }
+
+    public void moverY(int cy){
+
+    }
+
+    public void setX(int x){
+
+    }
+
+    public void setY(int y){}
+
+
+    @Override
+    public void inicializar() {
+
+    }
+
+    @Override
+    public void ejecutarFrame() {
+
+    }
+
+    @Override
+    public void finalizar() {
+
+    }
+}
