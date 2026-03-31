4798c605efcb3b4bc305a073969eac7bf72cdcc1
Skylus-98 <luissanchezmoreno04@gmail.com>
2026-03-31 18:03:09 +0200

    fondo

--- src/hlanz/juegonaves/Fondo.java
+++ src/hlanz/juegonaves/Fondo.java
@@ -1,6 +1,7 @@
 package hlanz.juegonaves;
 
 import bcp.framework.consola.GameObject;
+import bpc.daw.consola.CapaCanvas;
 
 import java.awt.*;
 
@@ -9,11 +10,47 @@ public class Fondo extends GameObject {
     private Image imagen;
     private Rectangle camara;
     private int velocidadCamara;
+    private int camaraX;
+    private int camaraY;
 
     // Constructor
     public Fondo(int vc){
+        this.imagen = Placeholder_fondo;
+        this.camara = new Rectangle();
         this.velocidadCamara = vc;
+        this.camaraX = 0;
+        this.camaraY = 0;
     }
 
     // Métodos
+    @Override
+    public void inicializar() {
+        this.camara = new Rectangle(0,0,this.juego.getAnchuraPantalla(),this.juego.getAlturaPantalla());
+    }
+
+    @Override
+    public void ejecutarFrame() {
+        CapaCanvas cc = this.consola.getCapaCanvas();
+        Graphics g = cc.getGraphics();
+        g.drawImage(
+                this.imagen,
+                0,0,
+                this.camara.width,this.camara.height,
+                this.camaraX, this.camaraY,
+                this.camaraX + this.camara.width, this.camaraY + this.camara.height,
+                null
+        );
+        this.camaraY += this.velocidadCamara;
+
+        if (this.camaraY + this.camara.height > this.imagen.getHeight(null)) {
+            this.camaraX = 0;
+            this.camaraY = 0;
+        }
+    }
+
+    @Override
+    public void finalizar() {
+    }
+
+
 }
