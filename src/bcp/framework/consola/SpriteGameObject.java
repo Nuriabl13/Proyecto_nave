58f70fb438bfa808d2b469b37588f26d270845df
Nuriabl13 <nuria13balderas2l@gmail.com>
2026-03-31 16:02:04 +0200

    Terminada la clase SpriteGameObject

--- src/bcp/framework/consola/SpriteGameObject.java
+++ src/bcp/framework/consola/SpriteGameObject.java
@@ -1,5 +1,6 @@
 package bcp.framework.consola;
 
+import bpc.daw.consola.Consola;
 import bpc.daw.consola.Sprite;
 
 import java.awt.*;
@@ -11,43 +12,49 @@ public class SpriteGameObject extends GameObject {
     private Point puntoInicial;
 
     public SpriteGameObject(Image img, int x, int y){
-
+        this.imagen = img;
+        this.puntoInicial = new Point(x,y);
+        this.sprite = null;
     }
 
     public int getX(){
-        return 0;
+        return this.puntoInicial.x;
     }
 
     public int getY(){
-        return 0;
+        return this.puntoInicial.y;
     }
 
     public int getAnchura(){
-        return 0;
+        return this.imagen.getWidth(null);
     }
 
     public int getAltura(){
-        return 0;
+        return this.imagen.getHeight(null);
     }
 
     public void moverX(int cx){
-
+        this.sprite.moverX(cx);
     }
 
     public void moverY(int cy){
-
+        this.sprite.moverY(cy);
     }
 
     public void setX(int x){
-
+        int diferencia = this.sprite.getX()-x;
+        this.sprite.moverX(diferencia);
     }
 
-    public void setY(int y){}
+    public void setY(int y){
+        int diferencia = this.sprite.getY()-y;
+        this.sprite.moverY(diferencia);
+    }
 
 
     @Override
     public void inicializar() {
-
+        this.sprite = super.consola.getCapaSprites().crearSprite(this.imagen,new Rectangle(this.puntoInicial),this.imagen.getWidth(null),this.imagen.getHeight(null));
     }
 
     @Override
@@ -57,6 +64,6 @@ public class SpriteGameObject extends GameObject {
 
     @Override
     public void finalizar() {
-
+        this.sprite = null;
     }
 }
