d58588ba46d43597339125da3e568b23dc1a50c1
Skylus-98 <luissanchezmoreno04@gmail.com>
2026-03-31 18:30:45 +0200

    Marcador

--- src/hlanz/juegonaves/Disparo.java
+++ src/hlanz/juegonaves/Disparo.java
@@ -30,6 +30,6 @@ public class Disparo extends SpriteGameObject {
         ){
             this.finalizar();
         }
-
+        // METODO INCOMPLETO
     }
 }
---
+++ src/hlanz/juegonaves/Marcador.java
@@ -0,0 +1,52 @@
+package hlanz.juegonaves;
+
+import bcp.framework.consola.GameObject;
+import bpc.daw.consola.CapaCanvas;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Marcador extends GameObject {
+    // Variables
+    private int[] puntos;
+
+    // Constructor
+    public Marcador(){
+        this.puntos = new int[]{0,0};
+    }
+
+    // Métodos
+    @Override
+    public void inicializar() {
+        this.puntos = new int[]{0,0};
+    }
+
+    public void incrementarPuntos(int jugador, int puntos) throws IllegalAccessException {
+        // JUGADOR 1 == 0  |  JUGADOR 2 == 1
+        if(puntos >= 0) {
+            if (jugador == 0) {
+                this.puntos[0] += puntos;
+            } else if (jugador == 1) {
+                this.puntos[1] += puntos;
+            } else {
+                throw new IllegalAccessException("ERROR: Número de JUGADOR INCORRECTO. J1 = 0, J2 = 1");
+            }
+        }else{
+            throw new IllegalAccessException("ERROR: La puntuación no puede ser negativa");
+        }
+    }
+
+    @Override
+    public void ejecutarFrame() {
+        CapaCanvas cc = this.consola.getCapaCanvas();
+        Graphics g = cc.getGraphics();
+        g.drawString(
+                "J1 : "+this.puntos[0]+"   |   J2 : "+this.puntos[1],
+                this.juego.getAnchuraPantalla()/2-30,0
+        );
+    }
+
+    @Override
+    public void finalizar() {
+    }
+}
