1ccd2069ff2585061eef777ede71e56d84af4626
Josemruizbros <josemanuelruizpalacios1@gmail.com>
2026-04-01 20:14:00 +0200

    CacheImagenes programado

--- src/hlanz/juegonaves/CacheImagenes.java
+++ src/hlanz/juegonaves/CacheImagenes.java
@@ -1,4 +1,45 @@
 package hlanz.juegonaves;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheImagenes {
+    //variables de la clase
+    private static CacheImagenes instancia = null;
+
+    //variables de instancia
+    private Map<String, Image> imagenes;
+
+    //constructores
+    private CacheImagenes() {
+        this.imagenes = new HashMap<>();
+    }
+
+    //métodos
+    public Image getImagen(String archivo) {
+        Image i = null;
+        if (this.imagenes.containsKey(archivo)) {
+            i = this.imagenes.get(archivo);
+        } else {
+            try {
+                i = ImageIO.read(new File(archivo));
+                this.imagenes.put(archivo, i);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return i;
+    }
+
+    //métodos de la clase
+    public static CacheImagenes getInstancia() {
+        if (CacheImagenes.instancia == null) {
+            CacheImagenes.instancia = new CacheImagenes();
+        }
+        return CacheImagenes.instancia;
+    }
 }
