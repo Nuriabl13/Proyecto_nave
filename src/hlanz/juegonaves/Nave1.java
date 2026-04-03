51fea2e1cc308f26f7a2505e17d374c4d11ad565
Josemruizbros <josemanuelruizpalacios1@gmail.com>
2026-04-03 12:34:24 +0200

    Naves programadas

--- src/hlanz/juegonaves/Nave1.java
+++ src/hlanz/juegonaves/Nave1.java
@@ -1,4 +1,15 @@
 package hlanz.juegonaves;
 
 public class Nave1 extends Nave {
+    //constructores
+    public Nave1(int ti, int tde, int tdi) {
+        super(ti, tde, tdi, 5, CacheImagenes.getInstancia().getImagen("recursos/nave1.png"));
+    }
+
+    //métodos del padre
+    @Override
+    protected void realizarDisparo() {
+        Disparo d = new Disparo(0, this.getX(), this.getY(), 0, -10);
+        this.escena.añadir(d);
+    }
 }
--- src/hlanz/juegonaves/Nave2.java
+++ src/hlanz/juegonaves/Nave2.java
@@ -1,4 +1,17 @@
 package hlanz.juegonaves;
 
 public class Nave2 extends Nave {
+    //constructores
+    public Nave2(int ti, int tde, int tdi) {
+        super(ti, tde, tdi, 3, CacheImagenes.getInstancia().getImagen("recursos/nave2.png"));
+    }
+
+    //métodos del padre
+    @Override
+    protected void realizarDisparo() {
+        Disparo d1 = new Disparo(1, this.getX(), this.getY(), -3, -3);
+        Disparo d2 = new Disparo(1, this.getX(), this.getY(), 3, -3);
+        this.escena.añadir(d1);
+        this.escena.añadir(d2);
+    }
 }
